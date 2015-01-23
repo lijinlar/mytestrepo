90b5d7025face3a97fbe553243b920ec9a5bc9a5
Lijin <lijin@baabte.com>
2015-01-23 18:55:39 +0530

    removed self notification

--- web/js/myScript.js
+++ web/js/myScript.js
@@ -48,7 +48,7 @@ $(document).ready(function(){
 			    	$('#chatwindow').html('');
 			    	$('#roomname').html($(this).html());
 			    	socketio.on('chat'+item.id,function(chatData){
-			    		//if(myId!=chatData.userId)
+			    		if(myId!=chatData.userId)
 			    			notifyMe(chatData);
 			    		var chatThread='<span class="chat-name">'+chatData.from+'</span>:<span class="text-muted">'+chatData.msg+'</span><br>';
 			    		$('#chatwindow').append(chatThread);
